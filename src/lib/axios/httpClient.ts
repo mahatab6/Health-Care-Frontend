fb95fa5370578fd5b38526be67e1120cabdd8980
mahatav6 <mahatabcall56@gmail.com>
2026-02-27 15:40:20 +0600

    axios data fecth added

---
+++ src/lib/axios/httpClient.ts
@@ -0,0 +1,99 @@
+import axios from 'axios'
+
+const API_BASE_URL= process.env.NEXT_pUBLIC_API_BASE_URL
+
+if(!API_BASE_URL) {
+    throw new Error("Base url not defined")
+}
+
+const axiosInstance = () => {
+    const instance = axios.create({
+        baseURL: API_BASE_URL,
+        timeout: 30000,
+        headers: {
+            'Content-Type' : 'application/json'
+        }
+    })
+    return instance;
+}
+
+export interface ApiRequestOptions {
+    params?: Record<string, unknown>;
+    headers: Record<string, string>;
+}
+
+const httpGet = async (endpoint: string, options?: ApiRequestOptions) => {
+    try {
+        const response = await axiosInstance().get(endpoint, {
+            params: options?.params,
+            headers: options?.headers,
+        })
+        return response.data;
+    } catch (error) {
+        console.error(`Get requset to ${endpoint} failed:`, error);
+        throw error;
+    }
+}
+
+const httpPost = async (endpoint:string, data: unknown, options?: ApiRequestOptions) => {
+    try {
+        const response = await axiosInstance().post(endpoint, data, {
+            params: options?.params,
+            headers: options?.headers,
+        });
+        return response.data;
+    } catch (error) {
+        console.error(`Post requst to ${endpoint} failed`, error);
+        throw error;
+    }
+}
+
+const httpPut = async (endpoint:string, data: unknown, options?: ApiRequestOptions) => {
+    try {
+        const response = await axiosInstance().put(endpoint, data, {
+            params: options?.params,
+            headers: options?.headers,
+        });
+        return response.data;
+    } catch (error) {
+        console.error(`Post requst to ${endpoint} failed`, error);
+        throw error;
+    }
+}
+
+const httpPatch = async (endpoint:string, data: unknown, options?: ApiRequestOptions) => {
+    try {
+        const response = await axiosInstance().patch(endpoint, data, {
+            params: options?.params,
+            headers: options?.headers,
+        });
+        return response.data;
+    } catch (error) {
+        console.error(`Post requst to ${endpoint} failed`, error);
+        throw error;
+    }
+}
+
+const httpDelete = async (endpoint:string, data: unknown, options?: ApiRequestOptions) => {
+    try {
+        const response = await axiosInstance().deljete(endpoint, {
+            params: options?.params,
+            headers: options?.headers,
+        });
+        return response.data;
+    } catch (error) {
+        console.error(`Post requst to ${endpoint} failed`, error);
+        throw error;
+    }
+}
+
+
+
+
+export const httpClient = {
+    get: httpGet,
+    post: httpPost,
+    put: httpPut,
+    patch: httpPatch,
+    delete: httpDelete
+}
