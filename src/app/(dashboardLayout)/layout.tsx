e543d8219a62bdff195ce686b99ce6a020ce253c
mahatav6 <mahatabcall56@gmail.com>
2026-03-06 06:02:11 +0600

    dashboard layout create

---
+++ src/app/(dashboardLayout)/layout.tsx
@@ -0,0 +1,26 @@
+import DashboardSidebar from "@/components/modules/Dashboard/DashboardSidebar"
+import React from "react"
+
+
+const RootDashboardLayout = async ({children} : {children: React.ReactNode}) => {
+  return (
+    <div className="flex h-screen overflow-hidden">
+        {/* Dashboard sidebar */}
+        <DashboardSidebar/>
+
+        <div className="flex flex-1 flex-col overflow-hidden">
+          {/* DashboardNavbar */}
+          <DashboardSidebar/>
+          {/* DashboardContent */}
+          <main className="flex-1 overflow-auto bg-muted/10 p-4 md:p-6">
+            <div>
+              {children}
+            </div>
+          </main>
+
+        </div>
+    </div>
+  )
+}
+
+export default RootDashboardLayout
